800015bfef70c56d5144abd81b6e8c24e51d14c8
zhz <zhanghongzhengtea@outlook.com>
2023-10-27 00:29:29 +0800

    在Person中增加了age属性

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @Auther:Zhanghz
+ * @Date: 2023/10/27 - 10 - 27 - 0:27
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
